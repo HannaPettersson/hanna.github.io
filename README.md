89e8fd17fd33719fca049792e3dc567d8da4647b
Hanna Pettersson <kontakta.hanna@gmail.com>
2018-06-15 13:38:16 +0200

    First code

--- README.md
+++
@@ -1,1 +0,0 @@
-# hannapettersson.github.io
---
+++ script.js
@@ -0,0 +1,5 @@
+// Write your Javascript excercises here:
+
+
+// What you want to print on your page goes here:
+document.querySelector(".result").innerHTML = "Hello world";
